dbb3cf2c45053577383b2645a52ee058ec6a03c3
juki3r <deepong25@gmail.com>
2026-08-25 19:36:22 +0800

    aa

---
+++ public/build/assets/input-error-BeJn7XrS.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-BqiA53gw.js";import{c as o}from"./button-C6kzi2z8.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
