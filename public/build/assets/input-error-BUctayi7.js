6b9d7ccf3974c013315e268fc5554504a05253c4
juki3r <deepong25@gmail.com>
2026-08-25 15:29:55 +0800

    aa

---
+++ public/build/assets/input-error-BUctayi7.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-g_gJbrZk.js";import{c as o}from"./button-Bcbjt2f7.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
