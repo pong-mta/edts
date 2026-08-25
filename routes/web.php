3a2174aa7a7fd240955c68a0a13fbcea4a8c8a9e
juki3r <deepong25@gmail.com>
2026-08-25 11:51:28 +0800

    aa

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/forgot-password/verify', function () {
     return Inertia::render('auth/verify-forgot-password');
 })->name('password.forgot.verify');
 
+Route::get('/reset-password', function () {
+    return Inertia::render('auth/reset-password');
+})->name('reset-password');
+
 Route::middleware(['auth'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
