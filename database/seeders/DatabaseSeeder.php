193faa3063bb68a4496cbb94fd25d231ff3cf992
juki3r <deepong25@gmail.com>
2026-08-25 09:59:10 +0800

    aa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,15 +6,12 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call([
+            DepartmentSeeder::class,
             RoleSeeder::class,
             AdminUserSeeder::class,
-            DepartmentSeeder::class,
         ]);
     }
 }
